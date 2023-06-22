5d5b01d77a7a04a8fbf704e5b13eeab00250fe3c
Sobeston <15335529+Sobeston@users.noreply.github.com>
2023-06-22 13:55:36 +0100

    add serde optional feature for serialisation

--- build.rs
+++ build.rs
@@ -17,14 +17,42 @@ fn main() {
             std::env::var(var).unwrap_or_else(|_| "unknown".into())
         );
     }
-    
-    let rustc_version = rustc_version::version_meta().expect("Failed to gather rustc version informatonn");
-    println!("cargo:rustc-env=RUMMAGE_RUSTC_VERSION_MAJOR={}",  rustc_version.semver.major);
-    println!("cargo:rustc-env=RUMMAGE_RUSTC_VERSION_MINOR={}",  rustc_version.semver.minor);
-    println!("cargo:rustc-env=RUMMAGE_RUSTC_VERSION_PATCH={}",  rustc_version.semver.patch);
-    println!("cargo:rustc-env=RUMMAGE_RUSTC_VERSION_PRE={}", rustc_version.semver.pre);
-    println!("cargo:rustc-env=RUMMAGE_RUSTC_VERSION_BUILD={}", rustc_version.semver.build);
-    println!("cargo:rustc-env=RUMMAGE_RUSTC_VERSION_COMMIT_HASH={}", rustc_version.commit_hash.unwrap_or_default());
-    println!("cargo:rustc-env=RUMMAGE_RUSTC_VERSION_COMMIT_DATE={}", rustc_version.commit_date.unwrap_or_default());
-    println!("cargo:rustc-env=RUMMAGE_RUSTC_VERSION_LLVM_VERSION={}", rustc_version.llvm_version.map(|l| format!("{}.{}", l.major, l.minor)).unwrap_or_default());
+
+    let rustc_version =
+        rustc_version::version_meta().expect("Failed to gather rustc version informatonn");
+    println!(
+        "cargo:rustc-env=RUMMAGE_RUSTC_VERSION_MAJOR={}",
+        rustc_version.semver.major
+    );
+    println!(
+        "cargo:rustc-env=RUMMAGE_RUSTC_VERSION_MINOR={}",
+        rustc_version.semver.minor
+    );
+    println!(
+        "cargo:rustc-env=RUMMAGE_RUSTC_VERSION_PATCH={}",
+        rustc_version.semver.patch
+    );
+    println!(
+        "cargo:rustc-env=RUMMAGE_RUSTC_VERSION_PRE={}",
+        rustc_version.semver.pre
+    );
+    println!(
+        "cargo:rustc-env=RUMMAGE_RUSTC_VERSION_BUILD={}",
+        rustc_version.semver.build
+    );
+    println!(
+        "cargo:rustc-env=RUMMAGE_RUSTC_VERSION_COMMIT_HASH={}",
+        rustc_version.commit_hash.unwrap_or_default()
+    );
+    println!(
+        "cargo:rustc-env=RUMMAGE_RUSTC_VERSION_COMMIT_DATE={}",
+        rustc_version.commit_date.unwrap_or_default()
+    );
+    println!(
+        "cargo:rustc-env=RUMMAGE_RUSTC_VERSION_LLVM_VERSION={}",
+        rustc_version
+            .llvm_version
+            .map(|l| format!("{}.{}", l.major, l.minor))
+            .unwrap_or_default()
+    );
 }
